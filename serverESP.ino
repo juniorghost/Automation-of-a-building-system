64f76309a8947b908665f67d2a8eee64bff0b3fc
Torrax <Carson.morris2@gmail.com>
2021-03-08 17:38:05 -0500

    Update serverESP.ino

--- serverESP.ino
+++ serverESP.ino
@@ -1,49 +1,110 @@
-// Import required libraries
+/////////////////////////////////////////////
+//                                         //
+//         Authors: Salim El Atache        //
+//             & Carson Morris             //
+//         Class: CAM8302E - 012           //
+//                 Lab: 5                  //
+//           Date: 07-03-2021              //
+//                                         //
+/////////////////////////////////////////////
+
+// Access Point Setup
 #include <ESP8266WiFi.h>
-#include "ESPAsyncWebServer.h"
-
-// Set your access point network credentials
-const char* ssid = "ESP8266-Access-Point";
-const char* password = "123456789";
-
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
-
-void setup(){
-  // Serial port for debugging purposes
-  Serial.begin(115200);
-  Serial.println();
-  
-  // Set up Access Point
-  Serial.print("Setting AP (Access Point)…");
-  WiFi.softAP(ssid, password); //Set up Access Point with Set SSID and Password
-
-  IPAddress IP = WiFi.softAPIP(); // Set the IP address by newly created Access Point
-  Serial.print("\nAP IP address: ");
-  Serial.println(IP);             // Print New IP address to Serial Monitor
-
-  server.on("/fan", HTTP_GET, [](AsyncWebServerRequest *request){     // When and HTTP Get Request is made to (IP)/temperature 
-    /////////RUN FUNCTION HERE////////////
-    request->send_P(200, "text/plain", "Is it working");                  // Return Message to Client With        
-  });
-  server.on("/heater", HTTP_GET, [](AsyncWebServerRequest *request){       // When and HTTP Get Request is made to (IP)/humidity
-    /////////RUN FUNCTION HERE////////////
-    request->send_P(200, "text/plain", "Heater ON");                  // Return Message to Client
-  });
-    server.on("/lock", HTTP_GET, [](AsyncWebServerRequest *request){     // When and HTTP Get Request is made to (IP)/temperature 
-    /////////RUN FUNCTION HERE////////////
-    request->send_P(200, "text/plain", "Lock ON");                  // Return Message to Client With        
-  });
-  server.on("/light", HTTP_GET, [](AsyncWebServerRequest *request){       // When and HTTP Get Request is made to (IP)/humidity
-    /////////RUN FUNCTION HERE////////////
-    request->send_P(200, "text/plain", "Light ON");                  // Return Message to Client
-  });
-  
-  bool status;
+const char WiFiPassword[] = "12345678";
+const char AP_NameChar[] = "ControlSystem" ;
  
-  server.begin();   // Start Server
-}
+WiFiServer server(80);
+
+// Webpage HTML Headings
+String header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+String html_1 = "<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1.0'/><meta charset='utf-8'><style>body {font-size:140%;} #main {display: table; margin: auto;  padding: 0 10px 0 10px; } h2,{text-align:center; } .button { padding:10px 10px 10px 10px; width:100%;  background-color: #4CAF50; font-size: 120%;}</style><title>Control System</title></head><body><div id='main'><h2>Control System</h2>";
+String html_2 = "";
+String html_3 = "";
+String html_4 = "";
+String html_5 = "</div></body></html>";
  
-void loop(){
-  
+String request = "";
+
+//Output Variables//
+#define fanRelay 12            // Fan Relay Digital Pin Number
+#define heatRelay 13           // Heat Relay Digital Pin Number
+#define lockRelay 14           // Lock Relay Digital Pin Number
+#define lightControl A0        // Light Triac Digital Pin Number
+ 
+void setup() 
+{
+  // Outputs //
+  pinMode(fanRelay, OUTPUT);         // Delclare fanRelay as an Output
+  digitalWrite(fanRelay, HIGH);      // Start fanRelay OFF
+  pinMode(heatRelay, OUTPUT);        // Delclare heatRelay as an Output
+  digitalWrite(heatRelay, HIGH);     // Start heatRelay OFF
+  pinMode(lockRelay, OUTPUT);        // Delclare lockRelay as an Output
+  digitalWrite(lockRelay, HIGH);     // Start lockRelay OFF
+ 
+    boolean conn = WiFi.softAP(AP_NameChar, WiFiPassword);
+    server.begin();
+ 
+} // void setup()
+ 
+ 
+ 
+void loop() 
+{
+    WiFiClient client = server.available();         // If Client is connected
+    if (!client)  {  return;  }
+ 
+    // Read the first line of the request
+    request = client.readStringUntil('\r');        //Get Request From Client
+ 
+    if       ( request.indexOf("fanON") > 0 )  {  //If Request Recieved "fanON"
+      digitalWrite(fanRelay, LOW);
+   } else if  ( request.indexOf("fanOFF") > 0 ) {  //If Request Recieved "fanOFF"
+        digitalWrite(fanRelay, HIGH);
+   } else if  ( request.indexOf("heaterON") > 0 ) {  //If Request Recieved "heaterON"
+    digitalWrite(heatRelay, LOW);
+    } else if  ( request.indexOf("heaterOFF") > 0 ) {  //If Request Recieved "fanOFF"
+      digitalWrite(heatRelay, HIGH);   
+   } else if  ( request.indexOf("lockON") > 0 ) {  //If Request Recieved "lockON"
+    digitalWrite(lockRelay, LOW);
+    } else if  ( request.indexOf("lockOFF") > 0 ) {  //If Request Recieved "lockOFF"
+      digitalWrite(lockRelay, HIGH);   
+/*    } else if  ( request.indexOf("lightON") > 0 ) {
+    digitalWrite(lightControl, LOW);
+    } else if  ( request.indexOf("lightOFF") > 0 ) {
+      digitalWrite(ightControl, HIGH);   */
+    } else{
+    }
+    
+ 
+ 
+    // Print HTML Buttons to Website
+    if (digitalRead(fanRelay) == LOW){                                                                                         //If Fan Relay is Currently ON
+       html_2 = "<form id='F1' action='fanOFF'><input class='button' type='submit' value='Turn OFF Fan' ></form><br>";         //Create Button for Turning OFF
+    } else {                                                                                                                   //Otherwise
+        html_2 = "<form id='F1' action='fanON'><input class='button' type='submit' value='Turn ON Fan' ></form><br>";          //Create Button for Turning ON
+    }
+    
+    if (digitalRead(heatRelay) == LOW) {                                                                                      //If Heat Relay is Currently ON
+       html_3 = "<form id='F2' action='heaterOFF'><input class='button' type='submit' value='Turn OFF Heater' ></form><br>";  //Create Button for Turning OFF
+    } else {                                                                                                                  //Otherwise
+        html_3 = "<form id='F2' action='heaterON'><input class='button' type='submit' value='Turn ON Heater' ></form><br>";   //Create Button for Turning ON
+    }
+
+    if (digitalRead(lockRelay) == LOW) {                                                                                     //If Lock Relay is Currently ON
+       html_4 = "<form id='F3' action='lockOFF'><input class='button' type='submit' value='Turn OFF Lock' ></form><br>";      //Create Button for Turning OFF
+    } else {                                                                                                                  //Otherwise
+        html_4 = "<form id='F3' action='lockON'><input class='button' type='submit' value='Turn ON Lock' ></form><br>";       //Create Button for Turning ON
+    }
+
+
+    client.flush();
+    // Print HTML Elements to Webpage
+    client.print( header );
+    client.print( html_1 );    
+    client.print( html_2 );
+    client.print( html_3 );
+    client.print( html_4);
+    client.print( html_5 );
+ 
+    delay(5); 
 }
